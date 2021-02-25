51a8622e62488cfb89386d3e20fc4675f2e966d1
github username <scott.gloyna01@gmail.com>
2021-02-24 20:58:10 -0600

    commit tree 960ce25de0d8e09603ab093352e71a20642bf0f2

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Add your code here
+
+// fetch('http://api.open-notify.org/astros.json') .then(function(response) {
+//   return response.json();
+// }).then(function(json) {
+//   console.log(json);
+//   console.log(`Holy cow! There are ${json["number"]} humans in space.`);
+// }); 
+
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     }); 
+
+
+
+
+
+function submitData(name,email) {
+
+    let formData = {
+            name: name,
+            email: email
+          };
+
+    let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users",configObj)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(object){
+            document.body.innerHTML = object.id;
+        })
+        .catch(function(error){
+            alert("Unauthorized Access");
+            document.body.innerHTML = error.message;
+        });
+}
+
+function addUserToDom(object){}
